5b45b477a800d69d119288e834748d763e4c3830
vidahoro2 <vidahoro@gmail.com>
2023-05-20 23:49:21 -0500

    Inicialización del proyecto

---
+++ react-course/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+    return (
+    <>
+      <div className='bg-yellow-500'>
+        
+        <p>Hablame papo</p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
